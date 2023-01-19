89bdd1e4fe806539425ee2ea1e9d9fd5bf25be5e
temujin469 <ttemujin469@gmail.com>
2023-01-19 23:35:57 +0800

    v1.0.0

--- frontend/src/utils/axios.js
+++ frontend/src/utils/axios.js
@@ -2,6 +2,6 @@ import axios from "axios";
 
 export const baseUrl = axios.create({
   // baseURL: "http://192.168.1.78:5000/api/v1",
-  baseURL: "https://tuko-tailor-api.vercel.app/api/v1",
+  baseURL: "https://tuko-tailor.vercel.app/api/v1",
 });
 //https://tuko-tailor-api.vercel.app/
